340b9b67996aecc0f3e7fe06c9263630ca82b06e
wu-jinpeng <wu.jinpeng@thoughtworks.com>
2023-02-06 21:07:25 +0800

    Refactor

--- src/main/java/com/example/demo/LoggingInterceptor.java
+++ src/main/java/com/example/demo/LoggingInterceptor.java
@@ -1,5 +1,9 @@
 package com.example.demo;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -16,9 +20,19 @@ public class LoggingInterceptor implements ExchangeFilterFunction {
     @Override
     public Mono<ClientResponse> filter(ClientRequest request, ExchangeFunction next) {
         return next.exchange(request)
-                .doOnSuccess(response -> {
-                    String logMessage = String.format("%s %s %s", request.method(), request.url(), response.statusCode());
-                    log.info(logMessage);
-                });
+                .flatMap(response -> response.bodyToMono(String.class)
+                        .doOnNext(responseBody -> {
+                            JsonNode jsonNode;
+                            try {
+                                jsonNode = new ObjectMapper().readTree(responseBody);
+                            } catch (JsonProcessingException e) {
+                                throw new RuntimeException(e);
+                            }
+
+                            ArrayNode data = (ArrayNode) jsonNode.get("data");
+                            String logMessage = String.format("%s %s %s %s", request.method(), request.url(), response.statusCode(), data.isEmpty() ? "❌" : "✅");
+                            log.info(logMessage);
+                        })
+                        .flatMap(responseBody -> Mono.just(response.mutate().body(responseBody).build())));
     }
 }
